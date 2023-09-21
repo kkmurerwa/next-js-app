8da956a17bc32db04e456488e33dac451ce32f16
Kenneth Murerwa <xwaxes@gmail.com>
2023-09-21 23:30:32 +0300

    feat: Add server-call fetches

--- app/conference/speakers/page.js
+++ app/conference/speakers/page.js
@@ -1,12 +1,32 @@
-import Link from "next/link";
+import styles from "../conference.module.css";
 
-export default function Page() {
+async function fetchSpeakers() {
+  const response = await fetch(
+    "https://raw.githubusercontent.com/adhithiravi/Consuming-GraphqL-Apollo/master/api/data/speakers.json",
+    { cache: "force-cache" }
+  );
+
+  const data = await response.json();
+  return data;
+}
+
+export default async function Page() {
+  const data = await fetchSpeakers();
+
   return (
-    <>
-      <h1>Welcome to Speakers Page</h1>
-      <h2>
-        <Link href="/conference">Back to Conference</Link>
-      </h2>
-    </>
+    <div className={styles.parentContainer}>
+      <div className="self-start whitespace-nowrap rounded-lg bg-gray-700 px-3 py-1 text-sm font-medium tabular-nums text-gray-100">
+        Last Rendered: {new Date().toLocaleTimeString()}
+      </div>
+      <h1>Welcome to Globomantics Speakers</h1>
+      {data.speakers.map(({ id, name, bio }) => {
+        return (
+          <div key={id} className={styles.infoContainer}>
+            <h3 className={styles.titleText}>{name}</h3>
+            <h5 className={styles.descText}>{bio}</h5>
+          </div>
+        );
+      })}
+    </div>
   );
 }
